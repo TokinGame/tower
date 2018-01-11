65d957fa7ce6e73f42ada436e2565077601cb776
Unknown <davimatyi@outlook.com>
2018-01-11 09:39:37 +0100

    héthúszpé

--- desktop/src/hu/tokingame/towerdefense/desktop/DesktopLauncher.java
+++ desktop/src/hu/tokingame/towerdefense/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package hu.tokingame.towerdefense.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import hu.tokingame.towerdefense.Globals.Globals;
 import hu.tokingame.towerdefense.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Globals.WORLD_WIDTH;
+		config.height = Globals.WORLD_HEIGHT;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
