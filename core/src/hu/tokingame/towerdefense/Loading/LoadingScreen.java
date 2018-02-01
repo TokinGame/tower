5a13346a317874d05cdaf9aa56e8df8da2f87ea0
Mátyás Dávid <davimatyi@outlook.com>
2018-02-01 19:49:36 +0100

    Loading screen

--- core/src/hu/tokingame/towerdefense/Loading/LoadingScreen.java
+++ core/src/hu/tokingame/towerdefense/Loading/LoadingScreen.java
@@ -1,12 +1,14 @@
 package hu.tokingame.towerdefense.Loading;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import hu.tokingame.towerdefense.Globals.Assets;
 import hu.tokingame.towerdefense.Globals.Globals;
 import hu.tokingame.towerdefense.Menu.MenuScreen;
 import hu.tokingame.towerdefense.MyBaseClasses.Scene2D.MyScreen;
+import hu.tokingame.towerdefense.MyBaseClasses.Scene2D.MyStage;
+import hu.tokingame.towerdefense.MyBaseClasses.Scene2D.OneSpriteAnimatedActor;
+import hu.tokingame.towerdefense.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 import hu.tokingame.towerdefense.MyGdxGame;
 
 
@@ -16,11 +18,36 @@ import hu.tokingame.towerdefense.MyGdxGame;
 
 public class LoadingScreen extends MyScreen {
 
+    MyStage stage;
+
+    private float elapsedTime = 0;
+    private OneSpriteStaticActor backGround;
+
 
     public LoadingScreen(MyGdxGame game) {
         super(game);
+        setBackGroundColor(0f, 0f, 0f);
+        stage = new MyStage(new ExtendViewport(Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT), spriteBatch, game){
+            @Override
+            public void init() {
+
+            }
+        };
+        //stage.addActor(backGround= new OneSpriteStaticActor("loadingbg.png"));
+//        backGround.setSize(Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT);
+  //      backGround.setPosition(0,0);
+        stage.addActor(new OneSpriteAnimatedActor("loading/loading.txt")
+        {
+            @Override
+            public void init() {
+                super.init();
+                setFps(30);
+                setSize(400, 400);
+                setPosition(Globals.WORLD_WIDTH/2-this.getWidth()/2, Globals.WORLD_HEIGHT/2-this.getHeight()/2);
+            }
+        });
     }
-    BitmapFont bitmapFont = new BitmapFont();
+
 
     @Override
     public void show() {
@@ -32,13 +59,15 @@ public class LoadingScreen extends MyScreen {
     public void render(float delta) {
         super.render(delta);
 
-        spriteBatch.begin();
-        bitmapFont.draw(spriteBatch,"Betöltés: " + Assets.manager.getLoadedAssets() + "/" + (Assets.manager.getQueuedAssets()+ Assets.manager.getLoadedAssets()) + " (" + ((int)(Assets.manager.getProgress()*100f)) + "%)", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        spriteBatch.end();
-        if (Assets.manager.update()) {
-            Assets.afterLoaded();
-            game.setScreen(new MenuScreen(game));
+        if (elapsedTime > 2.0 && Assets.manager.update()) {
+            if (Assets.manager.update()) {
+                Assets.afterLoaded();
+                game.setScreen(new MenuScreen(game));
+            }
         }
+        elapsedTime += delta;
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -50,4 +79,5 @@ public class LoadingScreen extends MyScreen {
     public void init() {
         setBackGroundColor(0f, 0f, 0f);
     }
+
 }
